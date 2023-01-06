7bd267ea132c1925d228164154655de8c6355a57
David Kroll <dkroll713@gmail.com>
2023-01-06 15:55:08 -0600

    began instructional readme

--- server/index.js
+++ server/index.js
@@ -13,19 +13,11 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   next();
 })
-
-app.use(express.json());
-
 app.options('*', cors());
-
+app.use(express.json());
 app.use(compression())
 app.use(minify())
 app.use(express.static(path.join(__dirname,"../public",)));
-
-app.post('/guess', (req,res) => {
-  console.log(req.body)
-  res.send('guess received')
-})
 
 app.get('/ping', ((req, res) => res.send(`pinged`)))
 
